3fcd01b5c490eda0e8f1a59b8ba748369a43215d
Emrullah DEMİR <e177demir@gmail.com>
2021-12-04 23:12:49 +0300

    Flask ve MySQL ile Blog Uygulaması

---
+++ main.py
@@ -0,0 +1,105 @@
+from flask import Flask, render_template, redirect, url_for, request, session
+import mysql.connector
+import timeago, datetime
+import hashlib
+
+app = Flask(__name__)
+app.secret_key = b"emrullahdemir"
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Ee10.dmr",
+    database="python"
+)
+cursor = db.cursor(dictionary=True)
+
+def categories():
+    sql = "SELECT * FROM categories ORDER BY category_name ASC"
+    cursor.execute(sql)
+    cats = cursor.fetchall()
+    return cats
+
+def timeAgo(date):
+    return timeago.format(date, datetime.datetime.now(), "tr")
+
+
+app.jinja_env.globals.update(cats=categories, timeAgo=timeAgo)
+app.jinja_env.filters["timeAgo"] = timeAgo
+
+@app.route("/newpost")
+def newPost():
+    return redirect(url_for("home"))
+
+@app.route("/")
+def home():
+    sql = "SELECT * FROM posts " \
+          "INNER JOIN users ON users.id = posts.post_user_id " \
+          "INNER JOIN categories ON categories.category_id = posts.post_category_id"
+    cursor.execute(sql)
+    posts = cursor.fetchall()
+    return render_template("index.html", posts=posts)
+
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+    error = ""
+    if request.method == "POST":
+        if request.form["email"] == "":
+            error = "E-posta adresinizi giriniz"
+        elif request.form["password"] == "":
+            error = "Şifrenizi giriniz"
+        else:
+            sql = "select * from users where email = %s && password = %s "
+            cursor.execute(sql, (request.form["email"],hashlib.md5((request.form["password"]).encode()).hexdigest(), ))
+            user = cursor.fetchone()
+            if user:
+                session["user_id"] = user["id"]
+                return  redirect(url_for("home"))
+            else:
+                error = "Kullanıcı bulunamadı..."
+    return render_template("login.html",error=error)
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/logout")
+def logout():
+    return redirect(url_for("home"))
+
+
+@app.route('/post/<url>')
+def post(url):
+    sql = "SELECT * FROM posts " \
+          "INNER JOIN users ON users.id = posts.post_user_id " \
+          "INNER JOIN categories ON categories.category_id = posts.post_category_id " \
+          "WHERE post_url = %s"
+    cursor.execute(sql, (url,))
+    post = cursor.fetchone()
+    if post:
+        return render_template('post.html', post=post)
+    else:
+        return redirect(url_for('home'))
+
+@app.route("/category/<url>")
+def category(url):
+
+    cursor.execute("select * from categories where category_url = %s", (url,))
+    categ = cursor.fetchone()
+
+    if categ:
+        sql = "SELECT * FROM posts " \
+              "INNER JOIN users ON users.id = posts.post_user_id " \
+              "INNER JOIN categories ON categories.category_id = posts.post_category_id " \
+              "WHERE post_category_id = %s " \
+              "ORDER BY post_id DESC "
+        cursor.execute(sql, (categ["category_id"],))
+        posts = cursor.fetchall()
+        return render_template("category.html", category=categ, posts=posts)
+    else:
+        return redirect(url_for("home"))
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('notfound.html'), 404
